62806a0407aad7d5ace6d74fb353df1ae39bf4a5
lorinczstefanmihai <54044259+lorinczstefanmihai@users.noreply.github.com>
2023-03-30 14:43:21 +0300

    am adaugat ultimele exercitii realizate

---
+++ 001HelloWorld/Debug/Src/subdir.mk
@@ -0,0 +1,30 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (10.3-2021.10)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Src/main.c \
+../Src/sysmem.c 
+
+OBJS += \
+./Src/main.o \
+./Src/sysmem.o 
+
+C_DEPS += \
+./Src/main.d \
+./Src/sysmem.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Src/%.o Src/%.su: ../Src/%.c Src/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m0plus -std=gnu11 -g3 -DDEBUG -DSTM32 -DSTM32G0 -DNUCLEO_G0B1RE -DSTM32G0B1RETx -c -I../Inc -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+
+clean: clean-Src
+
+clean-Src:
+	-$(RM) ./Src/main.d ./Src/main.o ./Src/main.su ./Src/sysmem.d ./Src/sysmem.o ./Src/sysmem.su
+
+.PHONY: clean-Src
+
